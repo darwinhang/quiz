62d11b5f674e88702f557201ac292662b2497bdc
Darwin Hang <darwin.hang@gmail.com>
2013-09-18 19:20:13 -0500

    added some files

---
+++ quiz2.js
@@ -0,0 +1,13 @@
+// quiz2.js
+// I need to improve the quiz app before I start
+// adding the other features
+
+/*	Create a user object that can store all the answers 
+	and the name of the user. */
+
+
+
+
+/* 	Create the question/button object, so that the user can go back
+	and forth between quesions. This object will also have
+	validation for answering questions	*/
